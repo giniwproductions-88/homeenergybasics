10bdf0361796b98f88b6cb9968e8ce3c1a3526b0
giniwproductions-88 <giniwproductions@gmail.com>
2026-03-17 00:27:35 -0500

    Add Maine to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -35,12 +35,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
-  url: `${baseUrl}/heat-pumps/states/mn`,
-  lastModified: new Date(),
-  changeFrequency: 'weekly',
-  priority: 0.8,
+      url: `${baseUrl}/heat-pumps/states/mn`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
     },
-
+    {
+      url: `${baseUrl}/heat-pumps/states/me`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
     {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
